9cf40081dbf2a361f3aa30d1acb9c053e0958170
wns312 <wns312@gmail.com>
2022-01-09 04:09:01 +0900

    test21

--- secret/decrypt_secret.sh
+++ secret/decrypt_secret.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output \
-$GITHUB_WORKSPACE/secret/.env \
-$GITHUB_WORKSPACE/secret/.env.gpg
+$GITHUB_WORKSPACE/secret/.env_test \
+$GITHUB_WORKSPACE/secret/.env_test.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output \
 $GITHUB_WORKSPACE/backend/runner/src/main/resources/firebase/firebase-notification-key.json \
