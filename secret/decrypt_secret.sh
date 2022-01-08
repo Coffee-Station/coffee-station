1ca31eccd2d979eeb0730d263c94d67adc21c9b4
wns312 <wns312@gmail.com>
2022-01-09 00:05:15 +0900

    test11

--- secret/decrypt_secret.sh
+++ secret/decrypt_secret.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output \
-$HOME/work/coffee-station/secret/.env \
-$HOME/work/coffee-station/secret/.env.gpg
+$HOME/work/coffee-station/coffee-station/secret/.env \
+$HOME/work/coffee-station/coffee-station/secret/.env.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output \
-$HOME/work/coffee-station/backend/runner/src/main/resources/firebase/firebase-notification-key.json \
-$HOME/work/coffee-station/backend/runner/src/main/resources/firebase/firebase-notification-key.json.gpg
+$HOME/work/coffee-station/coffee-station/backend/runner/src/main/resources/firebase/firebase-notification-key.json \
+$HOME/work/coffee-station/coffee-station/backend/runner/src/main/resources/firebase/firebase-notification-key.json.gpg
