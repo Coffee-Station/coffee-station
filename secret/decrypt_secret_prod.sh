dcca7eccbfdef12c6753c77314f65a9472e9331d
wns312 <wns312@gmail.com>
2022-01-23 17:34:52 +0900

    action test4

--- secret/decrypt_secret_prod.sh
+++ secret/decrypt_secret_prod.sh
@@ -4,6 +4,6 @@ gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE_PROD" --out
 $GITHUB_WORKSPACE/secret/.env \
 $GITHUB_WORKSPACE/secret/prod.env.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE_PROD" --output \
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE_LOCAL" --output \
 $GITHUB_WORKSPACE/backend/runner/src/main/resources/firebase/firebase-notification-key.json \
 $GITHUB_WORKSPACE/backend/runner/src/main/resources/firebase/firebase-notification-key.json.gpg
